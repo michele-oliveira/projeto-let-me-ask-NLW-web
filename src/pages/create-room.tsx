3626ea75e89258f85d479581fa478edd3b269288
michele-oliveira <micheleoliveiranovais24@gmail.com>
2025-07-11 11:46:36 -0300

    Integração banco de dados

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,10 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
+
+type GetRoomsAPIResponse = Array<{
+  id: string;
+  name: string;
+}>;
+
 export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-rooms"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3333/rooms");
+      const result: GetRoomsAPIResponse = await response.json();
+
+      return result;
+    },
+  });
+
   return (
     <div>
-      <div>Create Room</div>
-      <a className="underline" href="/room">
+      {isLoading && <p> Carregando...</p>}
+      <div>
+        {data?.map((room) => {
+          return <p key={room.id}>{room.name}</p>;
+        })}
+      </div>
+
+      <Link className="underline" to="/room">
         Acessar sala
-      </a>
+      </Link>
     </div>
   );
 }
