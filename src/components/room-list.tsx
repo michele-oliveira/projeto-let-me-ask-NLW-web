87e9841a6f47345feaf58e2479421f948e6b81a0
michele-oliveira <micheleoliveiranovais24@gmail.com>
2025-07-17 10:39:24 -0300

    Adicionando pastas e baixando input e textarea

--- src/components/room-list.tsx
+++ src/components/room-list.tsx
@@ -1,18 +1,19 @@
 import { ArrowRight } from "lucide-react";
+import { Link } from "react-router-dom";
+import { dayjs } from "@/lib/dayjs";
 import { Badge } from "./ui/badge";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardHeader, 
+  CardHeader,
   CardTitle,
 } from "./ui/card";
-import { Link } from "react-router-dom";
-import { dayjs } from "@/lib/dayjs";
 import { useRooms } from "@/http/use-rooms";
 
 export function RoomList() {
   const { data, isLoading } = useRooms();
+
   return (
     <Card>
       <CardHeader>
@@ -31,7 +32,7 @@ export function RoomList() {
             <Link
               className="flex items-center justify-between rounded-lg border p-3 hover:bg-accent/50"
               key={room.id}
-              to={`/rooms/${room.id}`}
+              to={`/room/${room.id}`}
             >
               <div className="flex flex-1 flex-col gap-1">
                 <h3 className="font-medium">{room.name}</h3>
@@ -45,6 +46,7 @@ export function RoomList() {
                   </Badge>
                 </div>
               </div>
+
               <span className="flex items-center gap-1 text-sm">
                 Entrar
                 <ArrowRight className="size-3" />
--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,15 +1,15 @@
-import { RoomList } from "@/components/room-list";
+import { CreateRoomForm } from '@/components/create-room-form'
+import { RoomList } from '@/components/room-list'
 
 export function CreateRoom() {
   return (
     <div className="min-h-screen px-4 py-8">
       <div className="mx-auto max-w-4xl">
-        <div className="grid-col-2 grid items-start gap-8">
-          <div />
-          <RoomList/>
+        <div className="grid grid-cols-2 items-start gap-8">
+          <CreateRoomForm />
+          <RoomList />
         </div>
       </div>
     </div>
-  );
+  )
 }
- 
